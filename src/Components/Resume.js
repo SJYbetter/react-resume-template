a2d0072c45f4b4db4351d6eef7f375d04f8ab11b
SJYbetter <sjyhyn@gmail.com>
2020-05-04 14:45:49 -0400

    v0

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -6,14 +6,14 @@ class Resume extends Component {
     if(this.props.data){
       var skillmessage = this.props.data.skillmessage;
       var education = this.props.data.education.map(function(education){
-        return <div key={education.school}><h3>{education.school}</h3>
+        return <div key={education.school}><h4>{education.school}</h4>
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
         </div>
       })
 
       //var workbullet = this.props.data.work
       var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
+        return <div key={work.company}><h4>{work.company}</h4>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <ul>
                 {work.description.split(";").map(item =>
@@ -30,9 +30,17 @@ class Resume extends Component {
 
         var publications = this.props.data.publications.map(function(publications){
             return <div key={publications.title}>
-                <h3><a href={publications.link}>{publications.title}</a></h3>
+                <h4><a href={publications.link}>{publications.title}</a></h4>
                 <p className="info">{publications.authors}</p>
                 <p>{publications.description}</p>
+            </div>
+        })
+
+
+        var awards = this.props.data.awards.map(function (awards) {
+            return <div key={awards.index}>
+                <h4>{awards.title}</h4>
+                <p>-{awards.years}</p>
             </div>
         })
     }
@@ -41,11 +49,11 @@ class Resume extends Component {
       <section id="resume">
 
       <div className="row education">
-         <div className="two columns header-col">
+         <div className="three columns header-col">
             <h1><span>Education</span></h1>
          </div>
 
-         <div className="ten columns main-col">
+         <div className="nine columns main-col">
             <div className="row item">
                <div className="twelve columns">
                  {education}
@@ -55,46 +63,52 @@ class Resume extends Component {
       </div>
 
           <div className="row work">
-              <div className="two columns header-col">
+              <div className="three columns header-col">
                   <h1><span>Publications</span></h1>
               </div>
-              <div className="ten columns main-col">
+              <div className="nine columns main-col">
                   {publications}
               </div>
           </div>
 
 
       <div className="row work">
-         <div className="two columns header-col">
+         <div className="three columns header-col">
             <h1><span>Experience</span></h1>
          </div>
-         <div className="ten columns">
+         <div className="nine columns">
           {work}
         </div>
     </div>
 
 
 
-
-
- {/*     <div className="row skill">
-
-         <div className="two columns header-col">
+    <div className="row skill">
+         <div className="three columns header-col">
             <h1><span>Skills</span></h1>
          </div>
-
-         <div className="ten columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
+         <div className="nine columns main-col">
 				<div className="bars">
 				   <ul className="skills">
 					  {skills}
 					</ul>
 				</div>
 			</div>
-      </div>*/}
+      </div>
+
+          <div className="row education">
+              <div className="three columns header-col">
+                  <h1><span>Award</span></h1>
+              </div>
+              <div className="nine columns main-col">
+                  <div className="row item">
+                      <ul className="twelve columns">
+                          {awards}
+                      </ul>
+                  </div>
+              </div>
+          </div>
+
    </section>
     );
   }
